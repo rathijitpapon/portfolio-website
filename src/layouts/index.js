b8e013318ecfbdf8c5920cb99ca2ab94bb00e156
Rathijit Paul <alienpapon009@gmail.com>
2020-12-11 14:51:37 +0600

    Add Lazy Import

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,23 +1,30 @@
-import React from "react";
+import React, {Suspense, lazy} from "react";
 import {Switch, Route, Redirect} from "react-router-dom";
 
 import Loading from "../views/Loading";
-import Home from "../views/Home";
-import Contact from "../views/Contact";
-import About from "../views/About";
-import Projects from "../views/Projects";
+// import Home from "../views/Home";
+// import Contact from "../views/Contact";
+// import About from "../views/About";
+// import Projects from "../views/Projects";
+
+const Home = lazy(() => import("../views/Home"));
+const Contact = lazy(() => import("../views/Contact"));
+const About = lazy(() => import("../views/About"));
+const Projects = lazy(() => import("../views/Projects"));
 
 const MainLayout = () => {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={Loading} />
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/project" component={Projects} />
-        {/* <Route exact path="/blog" component={Home} /> */}
-        <Route exact path="/about" component={About} />
-        <Route exact path="/contact" component={Contact} />
-        <Redirect path="*" to="/home" component={Home} />
+        <Suspense fallback={<Loading />}>
+          <Route exact path="/" component={Loading} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/project" component={Projects} />
+          {/* <Route exact path="/blog" component={Home} /> */}
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Redirect path="*" to="/home" component={Home} />
+        </Suspense>
       </Switch>
     </div>
   );
